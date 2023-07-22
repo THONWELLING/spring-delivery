49100f280e726fe1b562ab8ceac1bf42ef95e38e
THONWELLING <thonwellingdani13@gmail.com>
2023-07-22 00:05:44 -0300

    Creating The Kitchen And Restaurant Models

---
+++ src/main/java/com/thonwelling/fooddelivery/models/Kitchen.java
@@ -0,0 +1,41 @@
+package com.thonwelling.fooddelivery.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Kitchen implements Serializable {
+  @Serial
+  private static final long serialVersionUID =1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+  @Column(nullable = false)
+  private String name;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Kitchen kitchen)) return false;
+    return Objects.equals(getId(), kitchen.getId()) && Objects.equals(getName(), kitchen.getName());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getId(), getName());
+  }
+}
---
+++ src/main/java/com/thonwelling/fooddelivery/models/Restaurant.java
@@ -0,0 +1,43 @@
+package com.thonwelling.fooddelivery.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+public class Restaurant implements Serializable {
+  @Serial
+  private static final long serialVersionUID =1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private Double deliveryRate;
+
+  @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+  @JoinColumn(name = "kitchen_id")
+  private Kitchen kitchen;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Restaurant that)) return false;
+    return Objects.equals(getId(), that.getId()) && Objects.equals(getName(), that.getName()) && Objects.equals(getDeliveryRate(), that.getDeliveryRate());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getId(), getName(), getDeliveryRate());
+  }
+}
