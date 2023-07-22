afef36ebad984fec2d43331b7d64e6f0c54f2240
THONWELLING <thonwellingdani13@gmail.com>
2023-07-22 00:02:25 -0300

    Adsd Some Libs to Develop

---
+++ src/main/java/com/thonwelling/fooddelivery/StartUp.java
@@ -0,0 +1,11 @@
+package com.thonwelling.fooddelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUp {
+	public static void main(String[] args) {
+		SpringApplication.run(StartUp.class, args);
+	}
+}
