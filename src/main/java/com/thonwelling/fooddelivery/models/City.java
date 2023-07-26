7b092c5095bda32405620420f235409b69022cc6
THONWELLING <thonwellingdani13@gmail.com>
2023-07-26 02:25:20 -0300

    Updating City Entity Modifyinf relationship

--- src/main/java/com/thonwelling/fooddelivery/models/City.java
+++ src/main/java/com/thonwelling/fooddelivery/models/City.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -22,6 +23,23 @@ public class City implements Serializable {
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
   @Column(nullable = false)
   private String name;
+
+  @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+  @JoinColumn(name = "state_id", nullable = false)
+  private State State;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof City city)) return false;
+    return Objects.equals(getId(), city.getId()) && Objects.equals(getName(), city.getName()) && Objects.equals(getState(), city.getState());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getId(), getName(), getState());
+  }
 }
