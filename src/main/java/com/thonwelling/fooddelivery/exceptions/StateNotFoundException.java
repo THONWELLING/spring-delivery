13eb45566c40f25858fd6022b419736d1ee5fc7d
THONWELLING <thonwellingdani13@gmail.com>
2023-10-26 02:03:47 -0300

    Creating the Classes Diagram!

---
+++ src/main/java/com/thonwelling/fooddelivery/exceptions/StateNotFoundException.java
@@ -0,0 +1,15 @@
+package com.thonwelling.fooddelivery.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.Serial;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundEntityException extends RuntimeException {
+  @Serial
+  private static final long serialVersionUID = 1L;
+  public NotFoundEntityException(String message) {
+    super(message);
+  }
+}
